e23672846e6a965d84da29e8cdaa4741ba162254
umerfaisal <109797391+umerfaisal@users.noreply.github.com>
2024-06-26 16:09:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var transportationModes = ["Super Bikes", "Bus", "Hyper Car"];
+transportationModes.forEach(function (mode) { return console.log("I would like to own a ".concat(mode)); });
---
+++ main.ts
@@ -0,0 +1,2 @@
+let transportationModes= ["Super Bikes","Bus","Hyper Car"]
+transportationModes.forEach(mode => console.log(`I would like to own a ${mode}`));
